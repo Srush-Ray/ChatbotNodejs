8479d2f8aa5f21471d1840b47cfa846fdc25d18b
Srushti <srushti.raybhoge1999@gmail.com>
2021-05-01 13:23:15 +0530

    node to postgres heroku done

--- index.js
+++ index.js
@@ -23,18 +23,42 @@ io.on("connection", function (socket) {
   // console.log(socket.rooms);
   socket.on("chat message", function (msg) {
     console.log(msg);
-    const python = spawn("python", ["script.py", msg]);
-    var dataToSend;
-    python.stdout.on("data", function (data) {
-      dataToSend = data.toString();
-    });
+    // const python = spawn("python", ["script.py", msg]);
+    // var dataToSend;
+    // python.stdout.on("data", function (data) {
+    //   dataToSend = data.toString();
+    // });
+    request.post(
+      {
+        url: "http://127.0.0.1:5000/flask",
+        form: { message: msg, flag: "1" },
+      },
+      async function (error, response, body) {
+        console.error("error:", error); // Print the error
+        console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+        console.log("body:", body); // Print the data received
+        responseData = JSON.parse(body);
+        console.log(responseData.qid);
+        await pool.query(
+          `SELECT * FROM "Admin_query_table" where id=${responseData.qid}`,
+          (error, results) => {
+            if (error) {
+              throw error;
+            }
+            console.log(results.rows);
+          }
+        );
+        // res.send(body); //Display the response on the website
+        io.in(socket.id).emit("chat message", "from bot " + body);
+      }
+    );
     // in close event we are sure that stream from child process is closed
-    python.on("close", (code) => {
-      // send data to browser
-      // res.send(dataToSend);
-      console.log(dataToSend);
-      io.in(socket.id).emit("chat message", "from bot " + dataToSend);
-    });
+    // python.on("close", (code) => {
+    //   // send data to browser
+    //   // res.send(dataToSend);
+    //   console.log(dataToSend);
+    //   io.in(socket.id).emit("chat message", "from bot " + dataToSend);
+    // });
 
     // io.in(socket.id).emit("chat message", "from bot " + msg);
     // console.log("message: " + msg);
@@ -48,20 +72,23 @@ io.on("connection", function (socket) {
 });
 
 http.listen(port || 5000, function () {
-  console.log("listening on *:3000");
+  console.log("listening on *:" + port);
 });
 //////////////////////////////////////////////////////////////
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "chatbot",
-  password: "srushti6",
+  user: "dyhgctjqbmdgzi",
+  host: "ec2-23-22-191-232.compute-1.amazonaws.com",
+  database: "d8vvied9p5rnob",
+  password: "f42f02ba1dec14620f2ee83428f08c834f39edf81ed018d48669ebbfdbc4bb44",
   port: 5432,
+  ssl: true,
 });
 app.get("/allquestions", function (req, res) {
   // const id = parseInt(request.params.id);
 
-  pool.query('SELECT * FROM "Admin_query_table"', (error, results) => {
+  pool.query('SELECT * FROM "query_table"', (error, results) => {
     if (error) {
       throw error;
     }
@@ -78,7 +105,10 @@ app.get("/home", function (req, res) {
   //   res.send(body); //Display the response on the website
   // });
   request.post(
-    { url: "http://127.0.0.1:5000/flask", form: { key: "value" } },
+    {
+      url: "http://127.0.0.1:5000/flask",
+      form: { message: "value", flag: "0" },
+    },
     function (error, response, body) {
       console.error("error:", error); // Print the error
       console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
--- model.py
+++ model.py
@@ -1,39 +1,113 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import psycopg2
+import json
+# from asgiref.sync import async_to_sync
+# from .data_analysis import text_mining
+#from .Similarity import demo
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+import nltk
+import gensim
+from gensim import corpora
+from nltk.corpus import stopwords
+
 
 app = Flask(__name__)
 
 
 @app.route('/flask', methods=['POST'])
 def index():
-    conn = psycopg2.connect(database="chatbot", user="postgres",
-                            password="srushti6", host="127.0.0.1", port="5432")
-    print("Opened database successfully")
-    db_cursor = conn.cursor()
-    s = 'SELECT * FROM "Admin_query_table"'
+    asked_question = " "
     try:
-        # Execute the SQL
-        db_cursor.execute(s)
-        # Retrieve records from Postgres into a Python List
-        list_users = db_cursor.fetchall()
-    except psycopg2.Error as e:
-        t_message = "Database error: " + e + "/n SQL: " + s
-        return "Error"
+        conn = psycopg2.connect(database="chatbot", user="postgres",
+                                password="srushti6", host="127.0.0.1", port="5432")
+        asked_question = request.form['message']
+        # print("hey", request.args)
+        # user = request.args.get('key')
+        # print(user)
+        # text_data_json = json.loads(text_data)
+        # asked_question = text_data_json['message']
+        flag = request.form['flag']
+        message = request.form['message']
+        ans = 'not found'
+        ans_id = ''
+        # print(flag=="1")
+        if flag == "1":
+            t = None
+            #obj = demo()
+            que_list = []
+            ans_list = []
+            id_list = []
+            db_cursor = conn.cursor()
+            s = 'SELECT * FROM "Admin_query_table"'
+            try:
+                db_cursor.execute(s)
+                # Retrieve records from Postgres into a Python List
+                db_questions = db_cursor.fetchall()
+                for row in db_questions:
+                    # print(row[0])
+                    id_list.append(row[0])
+                    que_list.append(row[1])
+                    ans_list.append(row[2])
+                # print(que_list)
+                file2_docs = []
 
-    # Loop through the resulting list and print each user name, along with a line break:
-    for i in range(len(list_users)):
-        print(list_users[i])
+                gen_docs = [[w.lower() for w in word_tokenize(text)]
+                            for text in que_list]
+                dictionary = gensim.corpora.Dictionary(gen_docs)
+                corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs]
+                tf_idf = gensim.models.TfidfModel(corpus)
+                sims = gensim.similarities.Similarity(
+                    None, tf_idf[corpus], num_features=len(dictionary))
 
-    # Close the database cursor and connection
-    db_cursor.close()
-    conn.close()
+                tokens = sent_tokenize(asked_question)
+                for line in tokens:
+                    file2_docs.append(line)
 
-    user = request.form['key']
-    print(user)
-    # print("hey", request.args)
-    # user = request.args.get('key')
-    # print(user)
-    return "Me"
+                for line in file2_docs:
+                    query_doc = [w.lower() for w in word_tokenize(line)]
+                    query_doc_bow = dictionary.doc2bow(query_doc)
+
+                query_doc_tf_idf = tf_idf[query_doc_bow]
+                similarity_list = sims[query_doc_tf_idf]
+                sims.destroy()
+
+                max_similarity = max(similarity_list)
+
+                similarity_list = similarity_list.tolist()
+                max_similarity_index = similarity_list.index(max_similarity)
+
+                ans = ans_list[max_similarity_index]
+                ans_id = id_list[max_similarity_index]
+                print("answer id", ans_id)
+
+                # hold = Query_table.objects.get(id = ans_id)
+                # hold.viewed = hold.viewed + 1
+                # hold.save()
+
+            except psycopg2.Error as e:
+                t_message = "Database error: " + e + "/n SQL: " + s
+                return "Error in database query"
+            db_cursor.close()
+            conn.close()
+        # if flag == 0:
+        #     print("In flag =0")
+        #     t=None
+        #     find_name = text_mining(message)
+        #     name = find_name.identify_name()
+
+        #     if(name == '0'):
+        #         name = 'User'
+        #         flag = 2
+
+        response = {
+            "qid": ans_id,
+            "flag": "0"
+        }
+        # print(jsonify(response))
+        return jsonify(response), 200
+    except:
+        return "Lost connection to database try again"
 
 
 if __name__ == "__main__":
